a14fc9d4ada952b2287d1ad1147b979ec163703a
Caleb Muturi <calebmuturi2024@gmail.com>
2025-05-05 22:51:39 +0300

    Revert "feat(frontend): add Navbar component"
    This reverts commit 5f3c3bb2a83190e6fae7bdf078b9f3c43771fcbe.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import Navbar from './components/Navbar'
-
 export default function App() {
   return (
-    <div>
-      <Navbar />
-      <h1 className="text-3xl font-bold text-blue-600 mt-8 text-center">
-        AquaNexus Frontend Works!
-      </h1>
-    </div>
+    <h1 className="text-3xl font-bold text-blue-600">
+      AquaNexus Frontend Works!
+    </h1>
   )
 }

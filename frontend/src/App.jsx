9b83421c9e2acf82692e5b55b3f2053de3459543
Your User Name <joycekau2019@gmail.com>
2025-05-06 11:35:31 +0300

    feat(frontend): update App.jsx with latest changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from './components/Navbar'
-
+import Hero from './components/Hero'
+import HowItWorks from './components/HowItWorks'
+import Providers from './components/Providers'
+import Testimonials from './components/Testimonials'
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+<HowItWorks/>
+<Providers/>
+<Testimonials/>
       <h1 className="text-3xl font-bold text-blue-600 mt-8 text-center">
         AquaNexus Frontend Works!
       </h1>
